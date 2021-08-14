f7e0dd2569ed2dacc4a57bcfc43e288e07109fd7
fatihccetin <fatih.ccetin2@gmail.com>
2021-08-14 23:01:28 +0300

    addform

--- client/src/components/AddPostForm.jsx
+++ client/src/components/AddPostForm.jsx
@@ -1,138 +1,150 @@
-import React from 'react';
-import {makeStyles} from "@material-ui/core";
-import{
-    Button,
-    TextField,
-    Select,
-    Input,
-    MenuItem,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-
-}from "@material-ui/core";
-import { useFormControl,Controller } from "react-hook-form";
-import * as yup from"yup";
-import {yupResolver}from"@hookform/resolvers/yup";
-
+import React, { useState } from "react";
+import FileBase64 from "react-file-base64";
+import { useDispatch } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  TextField,
+  Select,
+  Input,
+  MenuItem,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { createPost } from "../actions/post";
 
 const useStyles = makeStyles((theme) => ({
-    paper:{
-        padding: theme.spaning(2)
-    },
-    textField:{
-        marginBottom:theme.spacing(2),
-    },
-
-
+  paper: {
+    padding: theme.spacing(2),
+  },
+  textField: {
+    marginBottom: theme.spacing(2),
+  },
 }));
 
-const tags = ["fun","programming","health","science"];
+const tags = ["fun", "programming", "health", "science"];
 
 const postSchema = yup.object().shape({
-        title: yup.string().required(),
-        subtitle: yup.string().required(),
-       content: yup.string().min(20).required(),
-        tag: yup.mixed().oneOf(tags),
+  title: yup.string().required(),
+  subtitle: yup.string().required(),
+  content: yup.string().min(20).required(),
+  tag: yup.mixed().oneOf(tags),
 });
 
+const AddPostForm = ({ open, handleClose }) => {
+  const dispatch = useDispatch();
 
- const AddPostForm = (open ,handleClose) => {
+  const [file, setFile] = useState(null);
+  const { register, handleSubmit, control, errors, reset } = useForm({
+    resolver: yupResolver(postSchema),
+  });
 
+  const onSubmit = (data) => {
+    dispatch(createPost({ ...data, image: file }));
+    clearForm();
+  };
 
-    const{register, handleSubmit, control , error , reset} = useForm({
-        resolve:yupResolver(postSchema)
-    })
-    const classes = useStyles();
+  const clearForm = () => {
+    reset();
+    setFile(null);
+    handleClose();
+  };
 
-    return  (
-        <Dialog open={open} onClose={handleClose}>
-            <DialogTitle >Yeni yazı olustur                    </DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Yeni Bir yazı eklemek için aşagıdakı formu doldurun.
-                    </DialogContentText>
-                    <div className={classes.root}>
-                        <form noValidate autoComplete="off" >
-                            <TextField
-                            id="title"
-                            label="Başlık"
-                            name="title"
-                            variant="outlined"
-                            className={classes.textField}
-                            size="small"
-                            inputRef={register}
-                            error={errors.title ? true : false}
-                            fullWidth
+  const classes = useStyles();
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle> Yeni Yazı Oluştur</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          To subscribe to this website, please enter your email address here. We
+          will send updates occasionally.
+        </DialogContentText>
+        <div className={classes.root}>
+          <form noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              id="title"
+              label="Başlık"
+              name="title"
+              variant="outlined"
+              className={classes.textField}
+              size="small"
+              inputRef={register}
+              error={errors.title ? true : false}
+              fullWidth
+            />
+            <TextField
+              id="subtitle"
+              label="Alt Başlık"
+              name="subtitle"
+              variant="outlined"
+              className={classes.textField}
+              size="small"
+              inputRef={register}
+              error={errors.subtitle ? true : false}
+              fullWidth
+            />
+            <Controller
+              as={
+                <Select
+                  input={<Input />}
+                  className={classes.textField}
+                  fullWidth
+                >
+                  {tags.map((tag, index) => (
+                    <MenuItem key={index} value={tag}>
+                      {tag}
+                    </MenuItem>
+                  ))}
+                </Select>
+              }
+              name="tag"
+              control={control}
+              error={errors.tag ? true : false}
+              defaultValue={tags[0]}
+            />
 
-                            /> 
-                            <TextField
-                            id="subtitle"
-                            label="Alt Başlık"
-                            name="subtitle"
-                            variant="outlined"
-                            className={classes.textField}
-                            size="small"
-                            inputRef={register}
-                            error={errors.subtitle ? true : false}
-                            fullWidth
-                            /> 
-                                <Controller
-                                   as={
-                                       <Select
-                                          input={<input/>}
-                                          className={classes.textField}
-                                          fullWidth
-                                       >
-                                           {
-                                               tags.map((tag , index) =>(
-                                                   <MenuItem key={index} value={tag}>
-                                                        {tag}  
-                                                   </MenuItem>
-                                               ))}
+            <TextField
+              id="content"
+              label="İçerik"
+              name="content"
+              multiline
+              size="small"
+              inputRef={register}
+              rows={4}
+              className={classes.textField}
+              variant="outlined"
+              error={errors.content ? true : false}
+              fullWidth
+            />
 
-
-                                       </Select>
-                                   }
-                                        name="tag"
-                                        control={control}
-                                        error={errors.tag ? true : false }
-                                        defaultValue={tags[0]}
-
-                                />
-                             <TextField
-                                id="content"
-                                label="İçerik"
-                                name="content"
-                                multiline
-                                rows={4}
-                                variant="outlined"
-                                className={classes.textField}
-                                size="small"
-                                inputRef={register}
-                                error={errors.content ? true : false}
-                                fullWidth
-                            /> 
-
-                        </form>
-                    </div>
-                </DialogContent>
-                <DialogActions>
-                    <Button color="inherit">Vazgec</Button>
-                    <Button type="submit" variant="autlined" color="primary">Yayınla</Button>
-                </DialogActions>
-
-        </Dialog>
-    )
-    
+            <FileBase64
+              multiple={false}
+              onDone={({ base64 }) => setFile(base64)}
+            />
+          </form>
+        </div>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={clearForm} color="inherit">
+          Vazgeç
+        </Button>
+        <Button
+          type="submit"
+          onClick={() => handleSubmit(onSubmit)()}
+          color="primary"
+          variant="outlined"
+        >
+          Yayınla
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 };
+
 export default AddPostForm;
-
-
-
-
-
-
-
